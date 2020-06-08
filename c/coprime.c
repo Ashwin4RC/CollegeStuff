06e072f1f3dbe738725497469aa1f827a1157d73
Ashwin R C <ashwin2001achu@gmail.com>
2020-06-08 09:00:54 +0530

    c: refactor coprime and gcd to use functions

--- c/coprime.c
+++ c/coprime.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
 
-int main() {
-    int n1,n2,i,gcd;
-    printf("Enter two numbers: \t");
-    scanf("%d %d",&n1,&n2);
+int gcd_checker(int n1,int n2){
+    int i,gcd;
     for(i=1;i<=n1 && i<=n2;i++){
         if(n1%i==0 && n2%i==0)
             gcd=i;
     }
-    gcd==1 ? printf("They are coprime \n") : printf("They arent coprime \n");
+    return gcd;
+}
+
+int main() {
+    int n1,n2;
+    printf("Enter two numbers: \t");
+    scanf("%d %d",&n1,&n2);
+    gcd_checker(n1,n2)==1 ? printf("They are coprime \n") : printf("They arent coprime \n");
     return 0;
 }
--- c/gcd_of_2.c
+++ c/gcd_of_2.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
 
-int main() {
-    int n1,n2,i,gcd;
-    printf("Enter two numbers: \t");
-    scanf("%d %d",&n1,&n2);
+int gcd_checker(int n1,int n2){
+    int i,gcd;
     for(i=1;i<=n1 && i<=n2;i++){
         if(n1%i==0 && n2%i==0)
             gcd=i;
     }
-    printf("Greatest common divisor is %d \n",gcd);
+    return gcd;
+}
+
+int main() {
+    int n1,n2;
+    printf("Enter two numbers: \t");
+    scanf("%d %d",&n1,&n2);
+    printf("Greatest common divisor is %d \n",gcd_checker(n1,n2));
     return 0;
 }
